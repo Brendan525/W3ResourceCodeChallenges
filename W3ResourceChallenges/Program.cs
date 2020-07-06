3b554fb56168ee84b01891d0c31e67c5b068d558
Brendan525 <broe5252@hotmail.com>
2020-07-05 20:29:49 -0400

    First commit

---
+++ W3ResourceChallenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W3ResourceChallenges
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
